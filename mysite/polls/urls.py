d91da7cf6f28fe70dfd0079d0aeab6d0f8cff9f0
Neilzon <neilcviloria@gmail.com>
2023-02-02 18:00:48 -0700

    more stuff

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('<int:question_id>/results/', views.IndexView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.IndexView.as_view(), name='vote'),
+    path('api/questions/', views.get_questions, name='get_questions'),
+    path('api/question/<int:pk>', views.update_question, name='update_question'),
 ]
